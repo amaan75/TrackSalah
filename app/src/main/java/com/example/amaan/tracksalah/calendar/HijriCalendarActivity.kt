93b167162fe4baa337daa5cdf991a39bddd652d2
coolaman75 <coolamangulati75@gmail.com>
2017-08-15 12:52:43 +0530

    Backup Before organising layout res folders.

--- app/src/main/java/com/example/amaan/tracksalah/calendar/HijriCalendarActivity.kt
+++ app/src/main/java/com/example/amaan/tracksalah/calendar/HijriCalendarActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.widget.FrameLayout
 import com.example.amaan.tracksalah.R
 import com.example.amaan.tracksalah.nav.BaseActivity
+import kotlinx.android.synthetic.main.activity_navigation_drawer.*
 import org.jetbrains.anko.find
 
 class HijriCalendarActivity : BaseActivity() {
@@ -14,5 +15,11 @@ class HijriCalendarActivity : BaseActivity() {
         //create a reference to content frame layout and load the layout file in the frame layout
         val contentFrameLayout = find<FrameLayout>(R.id.content_frame)
         layoutInflater.inflate(R.layout.activity_hijri_calendar, contentFrameLayout)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        //highlighting the calendar activity manually when it starts
+        nav_view.menu.findItem(R.id.nav_base_activity_hijri_calendar).isChecked = true
     }
 }
