be7840e7da7a311fd08fc324be79c24b83b2ef1b
coolaman75 <coolamangulati75@gmail.com>
2017-08-15 21:55:11 +0530

    Added the No Data Error Screen on Hijri Calendar

--- app/src/main/java/com/example/amaan/tracksalah/calendar/HijriDateFragment.kt
+++ app/src/main/java/com/example/amaan/tracksalah/calendar/HijriDateFragment.kt
@@ -21,7 +21,7 @@ class HijriDateFragment : ListFragment() {
         val rootView = inflater!!.inflate(R.layout.layout_hijri_date_fragment, container, false)
         val tempVals1: List<String> = listOf("AMAAN", "AMAAN KA BHAI", "USKI BAHEN", "USKE BETE")
         val tempVals = arrayListOf<String>()
-        val tempList = ArrayAdapter<String>(context, android.R.layout.simple_list_item_1, tempVals1)
+        val tempList = ArrayAdapter<String>(context, android.R.layout.simple_list_item_1, tempVals)
         val listView = rootView.findViewById<ListView>(android.R.id.list)
         listAdapter = tempList
         return rootView
